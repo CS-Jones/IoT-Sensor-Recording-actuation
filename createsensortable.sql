0d27bab17f97b2517e6814caabe27681f66b0265
CS-Jones <cjones.se@gmail.com>
2019-03-12 10:28:18 +0000

    Add files via upload

---
+++ createsensortable.sql
@@ -0,0 +1,9 @@
+use jonesc;
+DROP TABLE IF EXISTS sensorUsage; 
+CREATE TABLE sensorUsage (
+  UserID varchar(50),
+  SensorName varchar(50),
+  SensorValue varchar(20),
+  TimeInserted timestamp
+);
+  
